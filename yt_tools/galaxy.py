f7009d98a19729c9097436ae1757b417cb137a71
Gillen Brown <gillenbrown@gmail.com>
2018-04-25 15:22:10 -0400

    debugging

--- yt_tools/galaxy.py
+++ yt_tools/galaxy.py
@@ -1794,9 +1794,12 @@ class Galaxy(object):
         dx = data_obj[('index', 'dx')]
 
         good_idx = np.where(grid_levels == 6)
-        if len(good_idx) == 0 or len(dx) == 0:
-            return 100
-        dm_size = dx[good_idx][0]
+        # if len(good_idx) == 0 or len(dx) == 0:
+        #     return 100
+        try:
+            dm_size = dx[good_idx][0]
+        except IndexError:
+            print(dx, good_idx)
 
         return float(dm_size.to("pc").value)
 
